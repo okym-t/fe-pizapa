8513c88830035e5fe68544b2f84efcaff1176515
okym <cndd97@gmail.com>
2022-09-18 02:21:05 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import { Layout } from 'src/layout/layout'
 import Posts from './posts'
+import { NextPageWithLayout } from './_app'
 
-export const Top = () => {
+export const Top: NextPageWithLayout = () => {
   return <Posts />
 }
+
+Top.getLayout = (page) => <Layout>{page}</Layout>
 
 export default Top
