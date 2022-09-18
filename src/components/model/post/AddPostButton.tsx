3ab43f08812a966face2d39766a1bd9c5b3b4b13
okym <cndd97@gmail.com>
2022-09-19 04:29:08 +0900

    fix

--- src/components/model/post/AddPostButton.tsx
+++ src/components/model/post/AddPostButton.tsx
@@ -5,7 +5,7 @@ import NextLink from 'next/link'
 const AddPostButton = () => {
   return (
     <NextLink href='/posts/new' passHref>
-      <Button colorScheme='blue' w={100} size='md'>
+      <Button colorScheme='blue' size='md'>
         <AddIcon w={3} h={3} mr={1} />
         <Text>Add New</Text>
       </Button>
