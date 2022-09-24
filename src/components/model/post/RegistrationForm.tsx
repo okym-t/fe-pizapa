3ff33be59d8985f3678ac66db26d7caf5e652e62
okym <cndd97@gmail.com>
2022-09-25 00:35:54 +0900

    fix

--- src/components/model/post/RegistrationForm.tsx
+++ src/components/model/post/RegistrationForm.tsx
@@ -13,29 +13,42 @@ import { useRegisterPostForm } from 'src/hooks/useRegisterPostForm'
 import AddTagForm from './AddTagForm'
 
 const RegistrationForm: FC = () => {
-  const [register, handleSubmit, errors, onSubmit, tags, updateTags] =
-    useRegisterPostForm()
+  const [
+    register,
+    handleSubmit,
+    errors,
+    onSubmit,
+    isSubmitting,
+    tags,
+    updateTags,
+  ] = useRegisterPostForm()
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <Stack spacing={3}>
-        <FormControl isInvalid={errors.name !== undefined} isRequired>
+        <FormControl isInvalid={errors.name !== undefined}>
           <FormLabel htmlFor='name'>名前</FormLabel>
-          <Input bgColor='white' {...register('name')} />
+          <Input id='name' bgColor='white' {...register('name')} />
           <FormErrorMessage>{errors.name?.message}</FormErrorMessage>
         </FormControl>
         <FormControl>
           <Checkbox {...register('isAnonymous')}>匿名で投稿する</Checkbox>
         </FormControl>
-        <FormControl isInvalid={errors.title !== undefined} isRequired>
+        <FormControl isInvalid={errors.title !== undefined}>
           <FormLabel htmlFor='title'>タイトル</FormLabel>
-          <Input bgColor='white' maxLength={100} {...register('title')} />
+          <Input
+            id='title'
+            bgColor='white'
+            maxLength={100}
+            {...register('title')}
+          />
           <FormErrorMessage>{errors.title?.message}</FormErrorMessage>
         </FormControl>
         <AddTagForm tags={tags} updateTags={updateTags} />
-        <FormControl isInvalid={errors.description !== undefined} isRequired>
+        <FormControl isInvalid={errors.description !== undefined}>
           <FormLabel htmlFor='description'>内容</FormLabel>
           <Textarea
+            id='description'
             rows={10}
             bgColor='white'
             maxLength={1000}
@@ -45,7 +58,13 @@ const RegistrationForm: FC = () => {
           <FormErrorMessage>{errors.description?.message}</FormErrorMessage>
         </FormControl>
 
-        <Button w='100%' colorScheme='blue' variant='solid' type='submit'>
+        <Button
+          w='100%'
+          colorScheme='blue'
+          variant='solid'
+          type='submit'
+          isLoading={isSubmitting}
+        >
           登録
         </Button>
       </Stack>
